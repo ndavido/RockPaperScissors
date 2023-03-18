e8155d28fae291b82566c95067fa606953fdd65f
ndavido <D00239107@student.dkit.ie>
2023-03-18 20:53:51 +0200

    :sparkles: Added Join Session

--- main.py
+++ main.py
@@ -19,5 +19,28 @@ async def start_session():
     }
 
 @app.get("/api/join_session")
-async def join_session(sessionId, username):
-    return {}
+async def join_session(sessionId: str, username: str):
+    if sessionId not in sessions:
+        return {
+            "status": 0,
+            "message": "Cannot join a session. Session with this ID does not exist."
+        }
+    elif len(sessions[sessionId]["players"]) >= 2:
+        return {
+            "status": 0,
+            "message": "Cannot join a session. Session already has two players."
+        }
+    elif username in sessions[sessionId]["players"]:
+        return {
+            "status": 0,
+            "message": f"Cannot join a session. {username} has already joined the session."
+        }
+    else:
+        outcome = random.choice(["rock", "paper", "scissors"])
+        sessions[sessionId]["players"][username] = outcome
+        return {
+            "status": 1,
+            "message": f"{username} joined session {sessionId} successfully.",
+            "sessionId": sessionId,
+            "username": username
+        }
