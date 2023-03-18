ec6f6755248c6e755e2518f96d245fa6020d14a4
ndavido <D00239107@student.dkit.ie>
2023-03-18 20:45:13 +0200

    :sparkles: Added Start Session

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
+sessions = {}
+
 @app.get("/api/start_session")
 async def start_session():
-    return {}
+    sessionId = str(random.randint(100000, 999999))
+    sessions[sessionId] = {
+        "players": {},
+        "results": {}
+    }
+    return {
+        "status": 1,
+        "message": f"Session {sessionId} was created successfully.",
+        "sessionId": sessionId
+    }
 
 @app.get("/api/join_session")
 async def join_session(sessionId, username):
